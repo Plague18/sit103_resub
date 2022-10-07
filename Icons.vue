724995c923338bb09d23771d8b01abe47fd51961
Adhyan Mahajan <86746679+Plague18@users.noreply.github.com>
2022-10-07 17:04:35 +1100

    Add files via upload

---
+++ Icons.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <h5 class="title">100 Awesome Nucleo Icons</h5>
+      <p class="category">Handcrafted by our friends from <a href="https://nucleoapp.com/?ref=1712">NucleoApp</a></p>
+    </div>
+    <div class="card-body all-icons">
+      <div class="row">
+        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+          <div class="font-icon-detail">
+            <i class=""></i>
+            <p>icon-alert-circle-exc</p>
+          </div>
+
+        </div>
+        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+          <div class="font-icon-detail">
+            <i class=""></i>
+            <p>icon-align-center</p>
+          </div>
+
+        </div>
+        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+          <div class="font-icon-detail">
+            <i class=""></i>
+            <p>icon-align-left-2</p>
+          </div>
+
+        </div>
+        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+          <div class="">
+            <i class="tim-icons icon-app"></i>
+            <p>icon-app</p>
+          </div>
+
+        </div>
+
+        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+          <div class="">
+            <i class="tim-icons icon-atom"></i>
+            <p>icon-atom</p>
+
+          </div>
+
+        </div>
+
+        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+          <div class="">
+            <i class="tim-icons icon-attach-87"></i>
+            <p>icon-attach-87</p>
+          </div>
+
+        </div>
+        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+          <div class="">
+            <i class="tim-icons icon-badge"></i>
+            <p>icon-badge</p>
+          </div>
+
+       
+
+        </div>
+
+
+      
+
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+</style>
